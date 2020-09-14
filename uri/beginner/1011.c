6a0ff3d5ddb30530ffb5d742079a4d680a1a9ff0
Pedro H. C <pedrohcm5@gmail.com>
2020-09-13 21:37:05 -0300

    Update 1011.c

--- uri/beginner/1011.c
+++ uri/beginner/1011.c
@@ -3,11 +3,10 @@
 
 int main() {
 
-    double pi = 3.14159;
     double raio = 0; 
-    raio = scanf("%lf", &raio);
-    double volume = (4/3.0) * pi * pow(raio, 3);
-    printf("VOLUME = %.3f\n", volume);
+    scanf("%lf", &raio);
+    double volume = (4.0/3) * 3.14159 * pow(raio, 3);
+    printf("VOLUME = %.3lf\n", volume);
 
     return 0;
 }
